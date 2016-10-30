e4d5a290b5470695fa81f21f908f3864646a16ff
Francis Lawlor <franlawlor0@gmail.com>
2016-10-30 22:29:33 +0000

    Made subroutine for clearing screen.

--- 14461158_a3q1.asm
+++ 14461158_a3q1.asm
@@ -1,21 +1,28 @@
+# Student Name: Francis Lawlor
+# Student Number: 14461158
 
 		.data
-Pixels:		.space 262144
+Pixels:		.space 262144					# Allocate space for array to store words corresponding to pixel colours
 		.text						
 		.globl	main
 		
 main:
-		la	$t0, Pixels
-		li	$t1, 0
-		li	$t2, 65536
-		
-loop:
-		sw	$t1, 0($t0)
-		
-		addi	$t0, $t0, 4
-    		addi	$t1, $t1, 1
-    		
-    		bne $t1, $t2, loop 
-    		
+    		jal	ClearScreen				# Change pixels to white
     		li	$v0, 10
     		syscall
+
+ClearScreen:
+		la	$t0, Pixels				# Load address of array in which pixel colours are stored
+		li	$t1, 0					# Set counter to 0
+		li	$t2, 65536				# Store maximum value for counter
+		addi	$t3, $zero, 0xFFFFFF			# Store hexadecimal value for white in register $t3
+		
+loop:
+		sw	$t3, 0($t0)				# Store word corresponding to white colour in Pixels array
+		
+		addi	$t0, $t0, 4				# Increment to next elemen in Pixels array
+    		addi	$t1, $t1, 1				# Increment counter
+    		
+    		bne	$t1, $t2, loop				# While counter is less than 65536 continue to loop
+    		jr	$ra					# End subroutine
+		
