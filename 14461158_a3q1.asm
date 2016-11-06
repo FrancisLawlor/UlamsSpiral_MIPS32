7bb1849f00d95a6f6a0a4c57fec728af959f0ac0
Francis Lawlor <franlawlor0@gmail.com>
2016-11-06 01:31:23 +0000

    Added all the loops for grid navigation.

--- 14461158_a3q1.asm
+++ 14461158_a3q1.asm
@@ -4,13 +4,14 @@
 		.data
 Pixels:		.space 262144					# Allocate space for array to store words corresponding to pixel colours
 Primes:		.space 65025					# Allocate space for array to store prime numbers
+
 		.text						
 		.globl	main
 		
 main:
     		jal	ClearScreen				# Change pixels to white
     		jal	ClearMemory				# Clear memory in Primes array
-    		#jal	CalculatePrimes				# Calculate Primes
+    		jal	CalculatePrimes				# Calculate Primes
     		jal	DisplaySpiral				# Display Spiral
     		li	$v0, 10
     		syscall
@@ -28,6 +29,7 @@ loop:
     		addi	$t1, $t1, 1				# Increment counter
     		
     		bne	$t1, $t2, loop				# While counter is less than 65536 continue to loop
+    		
     		jr	$ra					# End subroutine
     		
 ClearMemory:
@@ -35,7 +37,7 @@ ClearMemory:
 		add	$s0, $s0, 2				# First two bytes are not populated, as per algorithm
 		addi	$s3, $zero, 0				# Store zero for use in storing zeros in FindPrime
 		addi	$s5, $zero, 2				# Store minimum value, 2 (used as counter)
-		addi	$s7, $zero, 1000			# Store maximum value, 1000 (used for guard)
+		addi	$s7, $zero, 65025			# Store maximum value, 1000 (used for guard)
 		
 StoreZero:	sb	$s3, ($s0)				# Store zero at current byte in $s0
 		addi	$s0, $s0, 1				# Move to next byte
@@ -45,47 +47,91 @@ StoreZero:	sb	$s3, ($s0)				# Store zero at current byte in $s0
 		jr	$ra					# End subroutine
 		
 CalculatePrimes:
-		# Setup before Sieve
-		addi	$s5, $zero, 2				# Store minimum value, 2 (used as counter for outer loop)
-		addi	$s7, $zero, 65025			# Store max value, 65025 (used for guard in outer loop)
-		addi	$t0, $zero, 1				# Store number 1 for (used to mark composite numbers in Primes, and in branching condition)
-		la	$s0, Primes				# Load address corresponding to Primes
-		add	$s0, $s0, 2				# Move to third byte, in keeping with algorithm
-
-OuterLoop:	beq	$s5, $s7, CalculatePrimes		# If counter reaches 65025, jump to Output
-
-		lb	$t6, 0($s0)				# Load current byte from Primes for branching condition	
-		beq	$t6, $t0, Increment			# If current byte contains 1 then jump to Increment
-		
-		addi	$t3, $zero, 2				# Store minimum value, 2 (used as counter for inner loop)
-		
-InnerLoop:	
-		# Setup before multiplication (repeated addition)
-		addi	$t2, $zero, 0				# Initialise counter to 0 (used to track number of additions)
-		addi	$t8, $zero, 0				# Store 0 in register (used to store total during repeated addition)
-		
-Multiply:	add	$t8, $t8, $t3				# Add current value of inner loop counter to current total from repeated addition
-		addi	$t2, $t2, 1				# Increment counter (used to track number of additions)
-		bne	$t2, $s5, Multiply			# Check if the counter used to track number of additions is equal to outer loop counter
-		
-		bge	$t8, $s7, Increment			# If multiple of counters is greater than or equal to max value, jump to Increment
-		
-		la	$t7, Primes				# Load address for Primes
-		add	$t7, $t7, $t8				# Add product of counters from inner loop and outer loop as offset from beginning of Primes
-		sb	$t0, ($t7)				# Save 1 to position in Primes corresponding to calculated offset
-		addi	$t3, $t3, 1				# Increment inner loop counter
-		j	InnerLoop
-		
-Increment:	addi	$s0, $s0, 1				# Move to next byte in Primes
-		addi	$s5, $s5, 1				# Increment outer loop counter
-		j	OuterLoop
+		la	$s0, Primes				# set data pointer
+		li      $s1, 2                  		# set the current base
+		li      $s2, 1                 	 		# strikeout constant
+		li      $s3, 1001              			# set end of array + 1
+		li      $s4, 501                		# set last base + 1
+loopBase:	add	$s5, $s1, $s1				# copy 2*base to the current value
+loopSweep:	add	$t0, $s0, $s5				# calculate the address of the current byte
+		sb      $s2, 0($t0)				# strike out the current byte
+		add	$s5, $s5, $s1				# increase the current value by the current base
+		blt	$s5, $s3, loopSweep			# repeat until current value > limit
+		addi	$s1, $s1, +1				# next base
+		blt	$s1, $s4, loopBase			# repeat until base > limit/2
 		
 		jr	$ra					# End subroutine
 		
 DisplaySpiral:
 		la	$t0, Pixels
-		addi	$t0, $t0, 131580
-		addi	$t3, $zero, 500				# Store hexadecimal value for white in register $t3
-		sw	$t3, 0($t0)				# Store word corresponding to white colour in Pixels array
+		addi	$t0, $t0, 131580			# Curr = 131580
+		addi	$t1, $zero, +1				# x = 1
+		add	$t2, $zero, 256				# Width
+		addi	$t4, $zero, 500				# Store hexadecimal value for white in register $t3
+		addi	$t5, $zero, 0				# Counter for iterating through Primes
+		la	$t6, Primes				# Load address for Primes array
+		#sw	$t3, 0($t0)				# Store word corresponding to white colour in Pixels array
+Outerloop:	
+
+		# Setup for LoopRight
+		addi	$t3, $zero, 0
+LoopRight:
+		## Do stuff
+		add	$t6, $t6, $t5
+		lb	$t7, 0($t6)
+		bne	$t7, 1, skip1
+		sw	$t4, 0($t0)
+		
+skip1:		addi	$t0, $t0, 4				# Move right.
+		addi	$t3, $t3, 1
+		addi	$t6, $t6, 1				# Increment Primes array
+		bne	$t3, $t1, LoopRight
+
+		# Setup for LoopUp
+		addi	$t3, $zero, 0
+LoopUp:
+		## Do stuff
+				
+		addi	$t0, $t0, -1024				# Move up.
+		addi	$t3, $t3, 1
+		bne	$t3, $t1, LoopUp
+
+		# Setup for LoopLeft
+		addi	$t1, $t1, 1				# x++
+		addi	$t3, $zero, 0
+		# TODO
+		
+		beq	$t1, 256, FinishSpiral
+LoopLeft:
+		## Do stuff
+		
+		#sw	$t4, 0($t0)
+		
+		addi	$t0, $t0, -4				# Move left.
+		addi	$t3, $t3, 1
+		bne	$t3, $t1, LoopLeft
+		
+		# Setup for LoopDown
+		addi	$t3, $zero, 0
+LoopDown:
+
+		## Do stuff
+		
+		addi	$t0, $t0, 1024				# Move left.
+		addi	$t3, $t3, 1
+		bne	$t3, $t1, LoopDown
+		
+		addi	$t1, $t1, 1
+		bne	$t1, $t2, Outerloop
+
+		addi	$t3, $zero, 0
+FinishSpiral:	
+
+		## Do stuff
+		sw	$t4, 0($t0)
+		
+		addi	$t0, $t0, -4				# Move left.
+		addi	$t3, $t3, 1
+		bne	$t3, $t1, FinishSpiral
 		
 		jr	$ra
